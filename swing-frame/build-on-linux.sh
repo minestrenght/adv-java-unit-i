0eaa6eb8ebf4dbda0b4c74862a22b5f100d6066b
minestrenght <you@example.com>
2014-07-14 12:54:49 +0530

    swing demos moved to unit ii

--- swing-frame/build-on-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo Compiling ur code ...
-javac Launch.java
-echo Press ENTER key to exit.
-read
--- swing-menu-item/run-on-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo Running ur code ...
-java Launch
-echo Press ENTER key to exit.
-read
